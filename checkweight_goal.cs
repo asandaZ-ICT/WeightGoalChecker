9402c1b14ebebab6c3af4edf09cd41582973c02c
Asanda Zondo <asandazondo884@gmail.com>
2025-08-28 03:52:17 +0200

    Add files via upload

---
+++ checkweight_goal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckWeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Write your current weight: " );
+            double currentweight = double.Parse(Console.ReadLine());
+
+            Console.Write("Write your goal weight: ");
+            double goalweight = double.Parse(Console.ReadLine());
+
+            double FinalMark = (currentweight) - (goalweight);
+            Console.WriteLine($"Weight to lose {FinalMark}Kgs");
+
+        }
+    }
+}
